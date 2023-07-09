f84bee63ac174255d86af7ae27b123a7be148a76
shimo8810 <shimo.8810@gmail.com>
2023-07-10 02:40:25 +0900

    [update] separate implementation of SSD and delay

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,166 +1,63 @@
 #![no_std]
 #![no_main]
 
+mod delay;
+mod ssd;
+
 use attiny_hal as hal;
-use embedded_hal::blocking::delay::DelayMs;
+
 use panic_halt as _;
 
-pub type Delay = hal::delay::Delay<hal::clock::MHz16>;
-
-pub fn delay_ms(ms: u16) {
-    Delay::new().delay_ms(ms);
-}
-
-const NUMS: [u8; 10] = [
-    0xFC, // 0
-    0x60, // 1
-    0xDA, // 2
-    0xF2, // 3
-    0x66, // 4
-    0xB6, // 5
-    0xBE, // 6
-    0xE0, // 7
-    0xFE, // 8
-    0xF6, // 9
-];
+use delay::delay_ms;
 
 #[hal::entry]
 fn main() -> ! {
     let dp = hal::Peripherals::take().unwrap();
-    let pins = hal::pins!(dp);
+    let pins: hal::Pins = hal::pins!(dp);
 
-    // digit pin
-    let mut d1 = pins.pd3.into_output();
-    let mut d2 = pins.pd4.into_output();
-    let mut d3 = pins.pd5.into_output();
-    let mut d4 = pins.pd6.into_output();
+    let mut ssd = ssd::Ssd::new(
+        pins.pd0.into_output(),
+        pins.pd1.into_output(),
+        pins.pd2.into_output(),
+        pins.pd3.into_output(),
+        pins.pd4.into_output(),
+        pins.pd5.into_output(),
+        pins.pd6.into_output(),
+        pins.pb0.into_output(),
+    );
 
-    // serial pin
-    let mut rclk = pins.pd0.into_output();
-    let mut ser = pins.pd1.into_output();
-    let mut srclk = pins.pd2.into_output();
-
-    rclk.set_low();
-    for _ in 0..8 {
-        srclk.set_low();
-        ser.set_low();
-        srclk.set_high();
-    }
-    rclk.set_high();
+    ssd.light_off();
 
     loop {
         for i in 0usize..10000 {
+            ssd.dot_on();
+            ssd.on_dig(4);
+            delay_ms(2);
+            ssd.light_off();
+
             let n0 = i / 1000;
-
-            // データ書き込み
-            rclk.set_low();
-            for s in 0..8 {
-                srclk.set_low();
-                if ((NUMS[n0] >> s) & 0x1) == 0 {
-                    ser.set_low();
-                } else {
-                    ser.set_high();
-                }
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d1.set_high();
-            delay_ms(3);
-
-            rclk.set_low();
-            for _ in 0..8 {
-                srclk.set_low();
-                ser.set_low();
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d1.set_low();
+            ssd.set_number(n0);
+            ssd.on_dig(0);
+            delay_ms(2);
+            ssd.light_off();
 
             let n1 = (i / 100) % 10;
-
-            // データ書き込み
-            rclk.set_low();
-            for s in 0..8 {
-                srclk.set_low();
-                if ((NUMS[n1] >> s) & 0x1) == 0 {
-                    ser.set_low();
-                } else {
-                    ser.set_high();
-                }
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d2.set_high();
-            delay_ms(3);
-
-            rclk.set_low();
-            for _ in 0..8 {
-                srclk.set_low();
-                ser.set_low();
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d2.set_low();
+            ssd.set_number(n1);
+            ssd.on_dig(1);
+            delay_ms(2);
+            ssd.light_off();
 
             let n2 = (i / 10) % 10;
-
-            // データ書き込み
-            rclk.set_low();
-            for s in 0..8 {
-                srclk.set_low();
-                if ((NUMS[n2] >> s) & 0x1) == 0 {
-                    ser.set_low();
-                } else {
-                    ser.set_high();
-                }
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d3.set_high();
-            delay_ms(3);
-
-            rclk.set_low();
-            for _ in 0..8 {
-                srclk.set_low();
-                ser.set_low();
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d3.set_low();
+            ssd.set_number(n2);
+            ssd.on_dig(2);
+            delay_ms(2);
+            ssd.light_off();
 
             let n3 = i % 10;
-
-            // データ書き込み
-            rclk.set_low();
-            for s in 0..8 {
-                srclk.set_low();
-                if ((NUMS[n3] >> s) & 0x1) == 0 {
-                    ser.set_low();
-                } else {
-                    ser.set_high();
-                }
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d4.set_high();
-            delay_ms(3);
-
-            rclk.set_low();
-            for _ in 0..8 {
-                srclk.set_low();
-                ser.set_low();
-                srclk.set_high();
-            }
-            rclk.set_high();
-
-            d4.set_low();
+            ssd.set_number(n3);
+            ssd.on_dig(3);
+            delay_ms(2);
+            ssd.light_off();
         }
     }
 }
